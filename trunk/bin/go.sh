5dc91da58ab04623da10ce55e9f53ce32464b628
hladky.jiri <hladky.jiri@0075613f-a23f-0410-a813-83caa6b13253>
2007-11-24 01:08:53 +0000

    go.sh: Main script - it will download the pages via wget, call make_one_html.pl to merge them into HTML and call toc.pl to create TOC
    make_one_html.pl: Convert many pages into one HTML document
    
    toc.pl: Create table of contens of HTML page

---
+++ trunk/bin/go.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+# vim: set fdm=marker: VIM modeline
+# Usage 
+# go.sh http://www.tomshardware.com/2007/11/19/the_spider_weaves_its_web
+
+# Colors {{{1
+function echo_red() {
+if [[ "$TERM" = linux || "$TERM" = xterm ]]
+then
+  echo -ne "\033[31m" 1>&2
+  echo $@ 1>&2
+  echo -ne "\033[0m" 1>&2
+else
+  echo $@ 1>&2
+fi
+}
+#}}}
+
+# WGET Options {{{1
+MAXIMUM_PAGES=60      #Maximum number of pages to download
+WGET_STANDARD=" -E -H -k -K -p --user-agent=Mozilla/5.0"
+WGET_EXPERIMENTAL=-"r -l1 --follow-tags=img"
+
+WGET_OPTIONS="$WGET_STANDARD $WGET_EXPERIMENTAL"
+
+#WGET_BASE_URL="http://www.tomshardware.com/2007/11/19/the_spider_weaves_its_web"
+WGET_BASE_URL=`echo $1 | perl -ne 'if (m%(.*)/$%) { print $1;}'`
+echo_red "URL is $WGET_BASE_URL"
+#}}}
+
+# Download pages {{{1
+echo_red "wget $WGET_OPTIONS ${WGET_BASE_URL}/index.html"
+
+if ! wget $WGET_OPTIONS ${WGET_BASE_URL}/index.html
+then
+  echo_red "Error calling wget $WGET_OPTIONS ${WGET_BASE_URL}/index.html"
+fi
+
+
+for ((a=1; a <= ${MAXIMUM_PAGES} ; a++)) 
+do 
+  echo_red "wget $WGET_OPTIONS ${WGET_BASE_URL}/page$a.html"
+  if ! wget $WGET_OPTIONS ${WGET_BASE_URL}/page$a.html
+  then
+    echo_red "Error calling wget $WGET_OPTIONS ${WGET_BASE_URL}/page$a.html"
+  fi
+done
+
+echo_red "Downloading is done!"
+#}}}
+
+# Convert pages into one file {{{1
+DATE=`date +%Y-%b-%d_%H.%M`
+WGET_NAME=`echo $WGET_BASE_URL | perl -ne "if (m%.*/%) { print $';}"`
+BASE_OUTPUT_NAME="${WGET_NAME}-${DATE}"
+BASE_OUTPUT_SUFFIX=".html"
+OUTPUT_NAME="${BASE_OUTPUT_NAME}${BASE_OUTPUT_SUFFIX}"
+DIRNAME=`echo $WGET_BASE_URL | perl -ne "if (m%http://%) { print $';}"`
+BIN_DIR=`dirname $0`
+BASE_DIR=`pwd`
+echo_red "Trying to cd $DIRNAME"
+cd $DIRNAME
+FILES=`ls page*html | sort -k 2 -n -t e`
+FILES="index.html $FILES"
+echo_red $FILES
+
+echo_red "Trying to call ${BASE_DIR}/${BIN_DIR}/make_one_html.pl"
+echo_red "Output will be written into `pwd`/${OUTPUT_NAME}"
+
+if ! ${BASE_DIR}/${BIN_DIR}/make_one_html.pl $FILES > ${OUTPUT_NAME}
+then
+  echo_red "Error calling ${BASE_DIR}/${BIN_DIR}/make_one_html.pl $FILES > ${OUTPUT_NAME}"
+  exit 1
+fi
+
+echo_red "Conversion is done!" 
+#}}}
+
+# Creating TOC {{{1
+echo_red "Trying to call ${BASE_DIR}/${BIN_DIR}/toc.pl ${OUTPUT_NAME}"
+
+if ! ${BASE_DIR}/${BIN_DIR}/toc.pl ${OUTPUT_NAME}
+then
+  echo_red "Error calling ${BASE_DIR}/${BIN_DIR}/toc.pl ${OUTPUT_NAME}"
+  exit 1
+fi
+
+# }}}
+
+# Calling firefox {{{1
+TOC_NAME="${BASE_OUTPUT_NAME}_frame${BASE_OUTPUT_SUFFIX}"
+if [ ! -r $TOC_NAME ] 
+then
+  echo_red "`pwd`/$TOC_NAME does not exist!"
+  OPEN_NAME=${OUTPUT_NAME}
+else
+  echo_red "`pwd`/$TOC_NAME does exist!"
+  OPEN_NAME=$TOC_NAME
+fi
+
+echo_red "Will try to open ${OPEN_NAME}"
+
+echo_red "Trying to call exec firefox `pwd`/${OPEN_NAME}"
+exec firefox `pwd`/${OPEN_NAME}
+# }}}
+
+# Some WGET Options descrition {{{1
+#-nr Don't remove the temporary .listing files
+#-k After the download is complete, convert the links in the document
+#-r recursive
+#-l Specify recursion maximum depth level
+#-np Do not ever ascend to the parent directory
+#-U,  --user-agent=AGENT    identify as AGENT instead of Wget/VERSION.
+#-p This option causes Wget to download all the files that are necessary to
+# properly display a given HTML page.  This includes such things as inlined images, sounds,
+# and referenced stylesheets.
+#}}}
---
+++ trunk/bin/make_one_html.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl -w
+# Usage feedthrou.pl index.html page*html > print.html
+
+sub count_div {
+  my $input_string=shift(@_);
+  my $check_string=$input_string;
+
+  while ($check_string =~ m/<div/) {
+    $DIV_COUNT+=1;
+    $check_string=$';
+  }
+  
+  $check_string=$input_string;
+  
+  while ($check_string =~ m%</div%) {
+    $DIV_COUNT-=1;
+    $check_string=$';
+  }
+  return $DIV_COUNT;
+}
+
+
+$DIV_COUNT=0;
+$FIRST=1;       #true;
+$TITLE=0;
+
+while(<>) {
+  if ($FIRST) { #First file in the list - we will look for title
+    print $_,"<html>\n" if ($.==1) ;
+    if ($TITLE == 0) {
+      if (m/<title/) {
+	print $&;
+	$_=$';
+	$TITLE=1;
+      }
+    }
+    if ($TITLE ==1) {
+      if (m%</title>%) {
+	print $`,$&,"\n";
+	$TITLE=-1;
+	$FIRST=0; #false
+      } else {
+	print;
+      }
+    }
+  }
+
+  if ( $.==1) {
+    print STDERR "Processing file $ARGV, still to process " , $#ARGV+1, " files.\n";
+  }
+    
+  if ( $DIV_COUNT==0) {
+    if (m/<div.*class="inner_content">/) {
+      print;
+      $DIV_COUNT=1;
+      $_=$';  #string following what was matched
+      $DIV_COUNT=count_div($_);
+    }
+  } else {
+    if (m/Digg this article/) {      
+      do {
+	print "</div>\n";
+	$DIV_COUNT-=1;
+      } until ( $DIV_COUNT==0);
+
+    } else {
+      $DIV_COUNT=count_div($_);
+      print;
+    }
+  }
+  if (eof) {
+    close (ARGV); #needed only when line counting should work for each input file separately
+    print "<!-- End of processing of file ", $ARGV, " -->\n";
+  }
+
+}
+
+print "</html>\n";
+
